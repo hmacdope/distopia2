b290fb83a840f510c0879bd08e63c442a7777a06
hmacdope <hugomacdermott@gmail.com>
2022-09-05 12:42:15 +1000

    add swizzles and tests

--- distopia/lib/simd_swizzles.h
+++ distopia/lib/simd_swizzles.h
@@ -3,33 +3,53 @@
 
 template <typename VectorT>
 inline void Deinterleave2(const VectorT a, const VectorT b, const VectorT c,
-                                   VectorT &x, VectorT &y, VectorT &z)
+                          VectorT &x, VectorT &y, VectorT &z)
 {
+    static_assert(ValuesPerPack<VectorT> == 2,
+                  "can only use to load into SIMD datatype of width 2");
+    // PRE: a = x0y0, b = z0,x1, c=y1,z1
+    x = blend2<0, 3>(a, b);
+    // x = x0x1
+    y = blend2<1, 2>(a, c);
+    // y = y0y1
+    z = blend2<0, 3>(b, c);
+    // z = z0,z1
 }
 template <typename VectorT>
 inline void Deinterleave4(const VectorT a, const VectorT b, const VectorT c,
-                                   VectorT &x, VectorT &y, VectorT &z)
+                          VectorT &x, VectorT &y, VectorT &z)
 {
+    static_assert(ValuesPerPack<VectorT> == 4,
+                  "can only use to load into SIMD datatype of width 4");
+
     // PRE: a = x0y0z0x1, b = y1z1x2y2, c = z2x3y3z3
-    //   __m128 t1 = shuffle_p<_MM_SHUFFLE(2, 1, 3, 2)>(b, c);
-    //   __m128 t2 = shuffle_p<_MM_SHUFFLE(1, 0, 2, 1)>(a, b);
-    // t1 = x2y2x3y3, t2 = y0z0y1z1
-    //   x = shuffle_p<_MM_SHUFFLE(2, 0, 3, 0)>(a, t1);
-    //   y = shuffle_p<_MM_SHUFFLE(3, 1, 2, 0)>(t2, t1);
-    //   z = shuffle_p<_MM_SHUFFLE(3, 0, 3, 1)>(t2, c);
-    // x = x0x1x2x3, y = y0y1y2y3, z = z0z1z2z3
+    VectorT t1 = blend4<2, 3, 5, 6>(b, c);
+    // t1 = x2y2x3y3,
+    VectorT t2 = blend4<1, 2, 4, 5>(a, b);
+    // t2 = y0z0y1z1
+
+    x = blend4<0, 3, 4, 6>(a, t1);
+    // x = x0x1x2x3
+    y = blend4<0, 2, 5, 7>(t2, t1);
+    // y = y0y1y2y3
+    z = blend4<1, 3, 4, 7>(t2, c);
+    // z = z0z1z2z3
 }
 
 template <typename VectorT>
 inline void Deinterleave8(const VectorT a, const VectorT b, const VectorT c,
-                                   VectorT &x, VectorT &y, VectorT &z)
+                          VectorT &x, VectorT &y, VectorT &z)
 {
+    static_assert(ValuesPerPack<VectorT> == 8,
+                  "can only use to load into SIMD datatype of width 8");
 }
 
 template <typename VectorT>
 inline void Deinterleave16(const VectorT a, const VectorT b, const VectorT c,
-                                    VectorT &x, VectorT &y, VectorT &z)
+                           VectorT &x, VectorT &y, VectorT &z)
 {
+    static_assert(ValuesPerPack<VectorT> == 16,
+                  "can only use to load into SIMD datatype of width 16");
 }
 
 #endif // DISTOPIA_SIMD_SWIZZLE_H
--- distopia/tests/tests.cpp
+++ distopia/tests/tests.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
+
 #include "../lib/example.h"
+#include "../lib/simd_swizzles.h"
 #include "../lib/vector_triple.h"
 
 using testing::Types;
 typedef Types<Vec4f, Vec8f, Vec16f, Vec2d, Vec4d, Vec8d> Implementations;
+typedef Types<Vec4f, Vec4d> Width4Implementations;
 
 template <typename T>
 class VectorTripleTest : public ::testing::Test
@@ -23,21 +26,21 @@ TYPED_TEST(VectorTripleTest, Construct)
 
     auto vt = VectorTriple<TypeParam>(x, x, x);
 
-    VectorToScalarT<TypeParam> buf0[ValuesPerPack<TypeParam>];
-    VectorToScalarT<TypeParam> buf1[ValuesPerPack<TypeParam>];
-    VectorToScalarT<TypeParam> buf2[ValuesPerPack<TypeParam>];
-    VectorToScalarT<TypeParam> buf3[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer0[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer1[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer2[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer3[ValuesPerPack<TypeParam>];
 
-    x.store(buf0);
-    vt.x.store(buf1);
-    vt.y.store(buf2);
-    vt.z.store(buf3);
+    x.store(out_buffer0);
+    vt.x.store(out_buffer1);
+    vt.y.store(out_buffer2);
+    vt.z.store(out_buffer3);
 
     for (int i = 0; i < vt.size; i++)
     {
-        ASSERT_FLOAT_EQ(buf0[i], buf1[i]);
-        ASSERT_FLOAT_EQ(buf0[i], buf2[i]);
-        ASSERT_FLOAT_EQ(buf0[i], buf3[i]);
+        ASSERT_FLOAT_EQ(out_buffer0[i], out_buffer1[i]);
+        ASSERT_FLOAT_EQ(out_buffer0[i], out_buffer2[i]);
+        ASSERT_FLOAT_EQ(out_buffer0[i], out_buffer3[i]);
     }
 }
 
@@ -57,10 +60,87 @@ TYPED_TEST(VectorTripleTest, LoadFromBuffer)
     vt.y.store(out_buffer2);
     vt.z.store(out_buffer3);
 
-        for (int j = 0; j < vt.size; j++)
-        {
-            ASSERT_FLOAT_EQ(input_buffer[j], out_buffer1[j]);
-            ASSERT_FLOAT_EQ(input_buffer[ValuesPerPack<TypeParam> +j], out_buffer2[j]);
-            ASSERT_FLOAT_EQ(input_buffer[ValuesPerPack<TypeParam>*2 +j], out_buffer3[j]);
-        }
+    for (int j = 0; j < vt.size; j++)
+    {
+        ASSERT_FLOAT_EQ(input_buffer[j], out_buffer1[j]);
+        ASSERT_FLOAT_EQ(input_buffer[ValuesPerPack<TypeParam> + j], out_buffer2[j]);
+        ASSERT_FLOAT_EQ(input_buffer[ValuesPerPack<TypeParam> * 2 + j], out_buffer3[j]);
+    }
+}
+
+// only Vec2d has width = 2 so no need for typed test
+TEST(Deinterleave2Test, Deinterleave)
+{
+    Vec2d a(0, 1);
+    Vec2d b(2, 3);
+    Vec2d c(4, 5);
+    Vec2d x;
+    Vec2d y;
+    Vec2d z;
+
+    double out_buffer1[2];
+    double out_buffer2[2];
+    double out_buffer3[2];
+
+    Deinterleave2(a, b, c, x, y, z);
+
+    x.store(out_buffer1);
+    y.store(out_buffer2);
+    z.store(out_buffer3);
+
+    ASSERT_DOUBLE_EQ(out_buffer1[0], 0);
+    ASSERT_DOUBLE_EQ(out_buffer1[1], 3);
+    ASSERT_DOUBLE_EQ(out_buffer2[0], 1);
+    ASSERT_DOUBLE_EQ(out_buffer2[1], 4);
+    ASSERT_DOUBLE_EQ(out_buffer3[0], 2);
+    ASSERT_DOUBLE_EQ(out_buffer3[1], 5);
+}
+
+template <typename T>
+class Deinterleave4Test : public ::testing::Test
+{
+public:
+    // blank, we do all the stuff in each test
+};
+
+TYPED_TEST_SUITE(Deinterleave4Test, Width4Implementations);
+
+TYPED_TEST(Deinterleave4Test, Deinterleave)
+{
+
+    TypeParam a(0, 1, 2, 3);
+    TypeParam b(4, 5, 6, 7);
+    TypeParam c(8, 9, 10, 11);
+    TypeParam x;
+    TypeParam y;
+    TypeParam z;
+
+    VectorToScalarT<TypeParam> out_buffer1[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer2[ValuesPerPack<TypeParam>];
+    VectorToScalarT<TypeParam> out_buffer3[ValuesPerPack<TypeParam>];
+
+    Deinterleave4(a, b, c, x, y, z);
+
+    x.store(out_buffer1);
+    y.store(out_buffer2);
+    z.store(out_buffer3);
+
+    // x expected = 0,3,6,9
+    ASSERT_FLOAT_EQ(out_buffer1[0], 0);
+    ASSERT_FLOAT_EQ(out_buffer1[1], 3);
+    ASSERT_FLOAT_EQ(out_buffer1[2], 6);
+    ASSERT_FLOAT_EQ(out_buffer1[3], 9);
+
+    // y expected = 1,4,7,10
+    ASSERT_FLOAT_EQ(out_buffer2[0], 1);
+    ASSERT_FLOAT_EQ(out_buffer2[1], 4);
+    ASSERT_FLOAT_EQ(out_buffer2[2], 7);
+    ASSERT_FLOAT_EQ(out_buffer2[3], 10);
+
+    // z expected = 2,5,8,11
+    ASSERT_FLOAT_EQ(out_buffer2[0], 2);
+    ASSERT_FLOAT_EQ(out_buffer2[1], 5);
+    ASSERT_FLOAT_EQ(out_buffer2[2], 8);
+    ASSERT_FLOAT_EQ(out_buffer2[3], 11);
+    
 }
