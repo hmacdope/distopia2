a86b6444bdebc61278295290b899e84e75f29bbd
hmacdope <hugomacdermott@gmail.com>
2022-09-26 10:53:19 +1000

    fix __init__

--- python/distopia/__init__.py
+++ python/distopia/__init__.py
@@ -1,4 +1,19 @@
 from .libdistopia import (
-    calc_bonds,
-    calc_bonds_idx,
+    calc_bonds_ortho_float,
+    calc_bonds_ortho_double,
+    calc_bonds_no_box_float,
+    calc_bonds_no_box_double,
+    calc_bonds_idx_ortho_float,
+    calc_bonds_idx_ortho_double,
+    calc_bonds_idx_no_box_float,
+    calc_bonds_idx_no_box_double
 )
+
+__all__ = ("calc_bonds_ortho_float",
+           "calc_bonds_ortho_double",
+           "calc_bonds_no_box_float",
+           "calc_bonds_no_box_double",
+           "calc_bonds_idx_ortho_float",
+           "calc_bonds_idx_ortho_double",
+           "calc_bonds_idx_no_box_float",
+           "calc_bonds_idx_no_box_double")
