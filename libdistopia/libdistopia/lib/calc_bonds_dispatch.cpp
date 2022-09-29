eb8aab0255cdb3e4f64472e990372f36839ffaa2
hmacdope <hugomacdermott@gmail.com>
2022-09-29 16:04:51 +1000

    dispatch

--- libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
+++ libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
@@ -6,10 +6,28 @@
 
 #include <cstddef>
 
-typedef float CalcBondsType(float const[]);
+typedef  void CalcBondsOrthoFT(const float *coords0, const float *coords1,
+                    const float *box, std::size_t n, float *out);
+
+
+// Define function prototypes for each version
+namespace Ns_SSE2{     // SSE2 instruction set
+    CalcBondsOrthoFT CalcBondsOrtho;
+};
+namespace Ns_AVX{      // AVX instruction set
+    CalcBondsOrthoFT myfunc;
+};
+namespace Ns_AVX2{     // AVX2 instruction set
+    CalcBondsOrthoFT myfunc;
+};
+namespace Ns_AVX512{   // AVX512 instruction set
+    CalcBondsOrthoFT myfunc;
+};
 
 // function prototypes for entry function and dispatcher, defined outside namespace
-CalcBondsType myfunc, myfunc_dispatch;
+CalcBondsOrthoFT CalcBondsOrthoF, CalcBondsOrthoFDispatch;
+
+CalcBondsOrthoDT CalcBondsOrthoD, CalcBondsOrthoDDispatch;
 
 /******************************************************************************
                              Dispatched code
@@ -21,31 +39,31 @@ each instruction set. Speed-critical vector code belongs here.
 namespace DISPATCHED_NAMESPACE
 {
 
-    // This section may contain vectors, functions, classes, objects, etc.
+//     // This section may contain vectors, functions, classes, objects, etc.
 
-    class MyClass
-    { // Just a silly example
-    public:
-        float sum(float const f[])
-        {                             // This function adds 16 floats
-            Vec16f a;                 // Vector of 16 floats
-            a.load(f);                // Load array into vector
-            return horizontal_add(a); // Return sum of 16 elements
-        }
-    };
+//     class MyClass
+//     { // Just a silly example
+//     public:
+//         float sum(float const f[])
+//         {                             // This function adds 16 floats
+//             Vec16f a;                 // Vector of 16 floats
+//             a.load(f);                // Load array into vector
+//             return horizontal_add(a); // Return sum of 16 elements
+//         }
+//     };
 
-    // -----------------------------------------------------------------------------
-    //                       Entry function
-    // -----------------------------------------------------------------------------
-    // This is the entry function that is accessed through the dispatcher.
-    // This serves as the interface between the common code and the dispatched code.
-    // The entry function cannot be member of a class.
-    // The entry function must use arrays rather than vectors for input and output.
-    float myfunc(float const f[])
-    {
-        MyClass myObject;
-        return myObject.sum(f);
-    }
+//     // -----------------------------------------------------------------------------
+//     //                       Entry function
+//     // -----------------------------------------------------------------------------
+//     // This is the entry function that is accessed through the dispatcher.
+//     // This serves as the interface between the common code and the dispatched code.
+//     // The entry function cannot be member of a class.
+//     // The entry function must use arrays rather than vectors for input and output.
+//     float myfunc(float const f[])
+//     {
+//         MyClass myObject;
+//         return myObject.sum(f);
+//     }
 }
 
 #if INSTRSET == 2
@@ -56,48 +74,48 @@ namespace DISPATCHED_NAMESPACE
 // ---------------------------------------------------------------------------------
 // This function pointer initially points to the dispatcher.
 // After the first call, it points to the selected version of the entry function
-MyFuncType *myfunc_pointer = &myfunc_dispatch; // function pointer
+CalcBondsOrthoFT *CalcBondsOrthoF_pointer = &CalcBondsOrthoFDispatch; // function pointer
 
 // Dispatch function
-float myfunc_dispatch(float const f[])
+void CalcBondsOrthoFDispatch(const float *coords0, const float *coords1,
+                    const float *box, std::size_t n, float *out)
 {
     int iset = instrset_detect(); // Detect supported instruction set
     // Choose which version of the entry function we want to point to:
     if (iset >= 9)
-        myfunc_pointer = &Ns_AVX512::myfunc; // AVX512
+        CalcBondsOrthoF_pointer = &Ns_AVX512::CalcBondsOrtho; // AVX512
     else if (iset >= 8)
-        myfunc_pointer = &Ns_AVX2::myfunc; // AVX2
+        CalcBondsOrthoF_pointer = &Ns_AVX2::CalcBondsOrtho; // AVX2
     else if (iset >= 7)
-        myfunc_pointer = &Ns_AVX::myfunc; // AVX
+        CalcBondsOrthoF_pointer = &Ns_AVX::CalcBondsOrtho; // AVX
     else if (iset >= 6)
-        myfunc_pointer = &Ns_SSE4_2::myfunc; // SSE4.2
+        CalcBondsOrthoF_pointer = &Ns_SSE4_2::CalcBondsOrtho; // SSE4.2
     else if (iset >= 5)
-        myfunc_pointer = &Ns_SSE4_1::myfunc; // SSE4.1
+        CalcBondsOrthoF_pointer = &Ns_SSE4_1::CalcBondsOrtho; // SSE4.1
     else if (iset >= 4)
-        myfunc_pointer = &Ns_SSSE3::myfunc; // SSSE3
+        CalcBondsOrthoF_pointer = &Ns_SSSE3::CalcBondsOrtho; // SSSE3
     else if (iset >= 3)
-        myfunc_pointer = &Ns_SSE3::myfunc; // SSE3
+        CalcBondsOrthoF_pointer = &Ns_SSE3::CalcBondsOrtho; // SSE3
     else if (iset >= 2)
-        myfunc_pointer = &Ns_SSE2::myfunc; // SSE2
+        CalcBondsOrthoF_pointer = &Ns_SSE2::CalcBondsOrtho; // SSE2
     else
     {
         // Error: lowest instruction set not supported.
         // Put any appropriate error handler here
         fprintf(stderr, "\nError: Instruction set SSE2 not supported on this computer");
-        return 0.f;
     }
     // continue in the dispatched version of the entry function
-    return (*myfunc_pointer)(f);
+    *CalcBondsOrthoF_pointer(coords0, coords1, box, n, out);
 }
 
 // Call the entry function through the function pointer.
-// The first time this function is called, it goes through the dispatcher.
-// The dispatcher will change the function pointer so that all subsequent
-// calls go directly to the optimal version of the entry function
-inline float myfunc(float const f[])
-{
-    return (*myfunc_pointer)(f); // go to dispatched version
-}
+// // The first time this function is called, it goes through the dispatcher.
+// // The dispatcher will change the function pointer so that all subsequent
+// // calls go directly to the optimal version of the entry function
+// inline float myfunc(float const f[])
+// {
+//     return (*myfunc_pointer)(f); // go to dispatched version
+// }
 #endif // INSTRSET == 2
 
 #endif // DISTOPIA_DISPATCH
