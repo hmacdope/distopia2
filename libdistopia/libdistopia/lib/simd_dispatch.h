c05bba8c65c64ccc9deb60b542b1d92b63582b38
hmacdope <hugomacdermott@gmail.com>
2022-10-03 12:02:07 +1100

    get namespacing right

--- libdistopia/libdistopia/lib/simd_dispatch.h
+++ libdistopia/libdistopia/lib/simd_dispatch.h
@@ -1,43 +1,111 @@
 #ifndef DISTOPIA_SIMD_DISPATCH_H
 #define DISTOPIA_SIMD_DISPATCH_H
 
-#ifdef DISTOPIA_DISPATCH // active only if we compile for dispatch
+#if DISTOPIA_DISPATCH // active only if we compile for dispatch
 
-#include "../../vcl2/vectorclass.h"
+// define so we can check mutual exclusivity
+#ifndef DISTOPIA_USE_SSE1
+#define DISTOPIA_USE_SSE1 0
+#endif
+
+#ifndef DISTOPIA_USE_SSE2
+#define DISTOPIA_USE_SSE2 0
+#endif
+
+#ifndef DISTOPIA_USE_SSE3
+#define DISTOPIA_USE_SSE3 0
+#endif
+
+#ifndef DISTOPIA_USE_SSSE3
+#define DISTOPIA_USE_SSSE3 0
+#endif
+
+#ifndef DISTOPIA_USE_SSE4_1
+#define DISTOPIA_USE_SSE4_1 0
+#endif
+
+#ifndef DISTOPIA_USE_SSE4_2
+#define DISTOPIA_USE_SSE4_2 0
+#endif
+
+#ifndef DISTOPIA_USE_AVX
+#define DISTOPIA_USE_AVX 0
+#endif
+
+#ifndef DISTOPIA_USE_AVX2
+#define DISTOPIA_USE_AVX2 0
+#endif
+
+#ifndef DISTOPIA_USE_AVX512
+#define DISTOPIA_USE_AVX512 0
+#endif
+
+
+
+// check SIMD options are  mutually exclusive
+// check we dont have overlapping defines
+
+static_assert(!DISTOPIA_USE_SSE2 || !DISTOPIA_USE_SSE1,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_SSE3 || !DISTOPIA_USE_SSE2,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_SSSE3 || !DISTOPIA_USE_SSE3,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_SSE4_1 || !DISTOPIA_USE_SSSE3,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_SSE4_2 || !DISTOPIA_USE_SSE4_1,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_AVX || !DISTOPIA_USE_SSE4_2,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_AVX2 || !DISTOPIA_USE_AVX,
+              "SIMD config is not self-consistent");
+static_assert(!DISTOPIA_USE_AVX512 || !DISTOPIA_USE_AVX2,
+              "SIMD config is not self-consistent");
+
 
 // Choose namespace name depending on which instruction set we compile for.
 
-/* The different instruction sets are defined in instrset_detect.cpp:
-2:  SSE2
-3:  SSE3
-4:  SSSE3 (Supplementary SSE3)
-5:  SSE4.1
-6:  SSE4.2
-7:  AVX
-8:  AVX2
-9:  AVX512F
-10: AVX512VL + AVX512BW + AVX512DQ
-*/
-
-#if INSTRSET >= 9 // AVX512
-#define DISPATCHED_NAMESPACE Ns_AVX512
-#elif INSTRSET >= 8 // AVX2
-#define DISPATCHED_NAMESPACE Ns_AVX2
-#elif INSTRSET >= 7 // AVX
-#define DISPATCHED_NAMESPACE Ns_AVX
-#elif INSTRSET >= 6 // SSE4.2
-#define DISPATCHED_NAMESPACE Ns_SSE4_2
-#elif INSTRSET >= 5 // SSE4.1
-#define DISPATCHED_NAMESPACE Ns_SSE4_1
-#elif INSTRSET >= 4 // SSSE3
-#define DISPATCHED_NAMESPACE Ns_SSSE3
-#elif INSTRSET >= 3 // SSE3
-#define DISPATCHED_NAMESPACE Ns_SSE3
-#elif INSTRSET == 2 // SSE2
-#define DISPATCHED_NAMESPACE Ns_SSE2
-#else
-#error Unsupported instruction set
+#if DISTOPIA_USE_SSE1
+#define DISPATCHED_NAMESPACE Ns_SSE1
 #endif
+
+#if DISTOPIA_USE_SSE2
+#define DISPATCHED_NAMESPACE Ns_SSE2
+#endif
+
+#if DISTOPIA_USE_SSE3
+#define DISPATCHED_NAMESPACE Ns_SSE3
+#endif
+
+#if DISTOPIA_USE_SSSE3
+#define DISPATCHED_NAMESPACE Ns_SSSE3
+#endif
+
+#if DISTOPIA_USE_SSE4_1
+#define DISPATCHED_NAMESPACE Ns_SSE4_1
+#endif
+
+#if DISTOPIA_USE_SSE4_2
+#define DISPATCHED_NAMESPACE Ns_SSE4_2
+#endif
+
+#if DISTOPIA_USE_SSE4_2
+#define DISPATCHED_NAMESPACE Ns_SSE4_2
+#endif
+
+
+#if DISTOPIA_USE_AVX
+#define DISPATCHED_NAMESPACE Ns_AVX
+#endif
+
+#if DISTOPIA_USE_AVX2
+#define DISPATCHED_NAMESPACE Ns_AVX2
+#endif
+
+#if DISTOPIA_USE_AVX512
+#define DISPATCHED_NAMESPACE Ns_AVX512
+#endif
+
 
 #endif  // DISTOPIA_DISPATCH
 
