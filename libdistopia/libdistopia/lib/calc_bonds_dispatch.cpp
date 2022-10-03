08c21ded785f2a71be24aa8fbf60d98daa9c0462
hmacdope <hugomacdermott@gmail.com>
2022-10-03 23:40:27 +1100

    add dispatch debugger

--- libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
+++ libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
@@ -1,6 +1,7 @@
 #if DISTOPIA_DISPATCH // active only if we compile for dispatch
 #if DISTOPIA_USE_SSE1 // only in lowest SIMD version
 
+#define DEBUG_DISPATCH 0
 
 #include "../include/distopia.h"
 #include "simd_dispatch.h"
@@ -18,72 +19,70 @@ typedef void CalcBondsOrthoDT(const double *coords0, const double *coords1,
 CalcBondsOrthoFT CalcBondsOrtho, CalcBondsOrthoDispatchF;
 CalcBondsOrthoDT CalcBondsOrtho, CalcBondsOrthoDispatchD;
 
+// global simd config
 
-// global simd config 
-
-constexpr simd_config _SIMD_config = simd_config();  
-
+constexpr simd_config _SIMD_config = simd_config();
 
 // Define function prototypes
 namespace Ns_SSE1
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_SSE2
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_SSE3
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_SSSE3
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_SSE4_1
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_SSE4_2
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 namespace Ns_AVX
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 namespace Ns_AVX2
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 namespace Ns_AVX512
 {
-template <typename T>
-void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
-                    std::size_t n, T *out);
+    template <typename T>
+    void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                        std::size_t n, T *out);
 };
 
 CalcBondsOrthoFT *CalcBondsOrthoF_pointer = &CalcBondsOrthoDispatchF; // function pointer
@@ -93,168 +92,173 @@ CalcBondsOrthoDT *CalcBondsOrthoD_pointer = &CalcBondsOrthoDispatchD; // functio
 // branches are on _SIMD_config are constexpr so should be trimmed nicely
 // by the compiler
 void CalcBondsOrthoDispatchF(const float *coords0, const float *coords1,
-                            const float *box, std::size_t n, float *out)
+                             const float *box, std::size_t n, float *out)
 {
+#if DEBUG_DISPATCH
     printf("function dispatch: float\n");
-    printf("SIMDAVX512 %i\n",_SIMD_config.has_AVX512);
-    printf("SIMDAVX2 %i\n",_SIMD_config.has_AVX2);
-    printf("SIMDAVX %i\n",_SIMD_config.has_AVX);
-    printf("SIMDSSE3 %i\n",_SIMD_config.has_SSE3);
-    printf("SIMDSSE1 %i\n",_SIMD_config.has_SSE1);
+    printf("SIMD AVX512 %i\n", _SIMD_config.has_AVX512);
+    printf("SIMD AVX2 %i\n", _SIMD_config.has_AVX2);
+    printf("SIMD AVX %i\n", _SIMD_config.has_AVX);
+    printf("SIMD SSE4_2 %i\n", _SIMD_config.has_SSE4_2);
+    printf("SIMD SSE4_1%i\n", _SIMD_config.has_SSE4_1);
+    printf("SIMD SSSE3 %i\n", _SIMD_config.has_SSSE3);
+    printf("SIMD SSE3 %i\n", _SIMD_config.has_SSE3);
+    printf("SIMD SSE2 %i\n", _SIMD_config.has_SSE1);
+    printf("SIMD SSE1 %i\n", _SIMD_config.has_SSE1);
+#endif
 
     int iset = instrset_detect(); // Detect supported instruction set
     // Choose which version of the entry function we want to point to:
 
+#if DEBUG_DISPATCH
     printf("ISET: %i\n", iset);
+#endif
+
     if (iset >= 9 && _SIMD_config.has_AVX512)
     {
-        #if DISTOPIA_AVX512_AVAILABLE
-        printf("\nRuntime dispatch AVX512\n");
+#if DISTOPIA_AVX512_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_AVX512::CalcBondsOrtho; // AVX512
-        #endif
-
+#endif
     }
-    else if  (iset >= 8 && _SIMD_config.has_AVX2)
+    else if (iset >= 8 && _SIMD_config.has_AVX2)
     {
-        #if DISTOPIA_AVX2_AVAILABLE
-        printf("\nRuntime dispatch AVX2\n");
+#if DISTOPIA_AVX2_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_AVX2::CalcBondsOrtho; // AVX2
-        #endif
+#endif
     }
     else if (iset >= 7 && _SIMD_config.has_AVX)
     {
-       #if DISTOPIA_AVX_AVAILABLE
-        printf("\nRuntime dispatch AVX\n");
+#if DISTOPIA_AVX_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_AVX::CalcBondsOrtho; // AVX
-       #endif
+#endif
     }
     else if (iset >= 6 && _SIMD_config.has_SSE4_2)
     {
-        #if DISTOPIA_SSE4_2_AVAILABLE
-        printf("\nRuntime dispatch SSE4.2\n");
+#if DISTOPIA_SSE4_2_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSE4_2::CalcBondsOrtho; // SSE4.2
-        #endif
+#endif
     }
     else if (iset >= 5 && _SIMD_config.has_SSE4_1)
     {
-        #if DISTOPIA_SSE4_1_AVAILABLE
-        printf("\nRuntime dispatch SSE4.1\n");
+#if DISTOPIA_SSE4_1_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSE4_1::CalcBondsOrtho; // SSE4.1
-        #endif
+#endif
     }
     else if (iset >= 4 && _SIMD_config.has_SSSE3)
     {
-        #if DISTOPIA_SSSE3_AVAILABLE
-        printf("\nRuntime dispatch SSSE3\n");
+#if DISTOPIA_SSSE3_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSSE3::CalcBondsOrtho; // SSSE3
-        #endif
+#endif
     }
     else if (iset >= 3 && _SIMD_config.has_SSE3)
     {
-        #if DISTOPIA_SSE3_AVAILABLE 
-        printf("\nRuntime dispatch SSE3\n");
+#if DISTOPIA_SSE3_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSE3::CalcBondsOrtho; // SSE3
-        #endif
+#endif
     }
     else if (iset >= 2 && _SIMD_config.has_SSE2)
     {
-        #if DISTOPIA_SSE2_AVAILABLE 
-        printf("\nRuntime dispatch SSE2\n");
+#if DISTOPIA_SSE2_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSE2::CalcBondsOrtho; // SSE2
-        #endif
+#endif
     }
     else if (iset >= 1 && _SIMD_config.has_SSE1)
     {
-        // this should always be the case
-        #if DISTOPIA_SSE1_AVAILABLE
-        printf("\nRuntime dispatch SSE1\n");
+// this should always be the case
+#if DISTOPIA_SSE1_AVAILABLE
         CalcBondsOrthoF_pointer = &Ns_SSE1::CalcBondsOrtho; // SSE1
-        #endif
+#endif
     }
     // continue in the dispatched version of the entry function
     (*CalcBondsOrthoF_pointer)(coords0, coords1, box, n, out);
 }
 
-
 // Dispatch function
 void CalcBondsOrthoDispatchD(const double *coords0, const double *coords1,
-                    const double *box, std::size_t n, double *out)
+                             const double *box, std::size_t n, double *out)
 {
+#if DEBUG_DISPATCH
     printf("function dispatch: double\n");
+    printf("SIMD AVX512 %i\n", _SIMD_config.has_AVX512);
+    printf("SIMD AVX2 %i\n", _SIMD_config.has_AVX2);
+    printf("SIMD AVX %i\n", _SIMD_config.has_AVX);
+    printf("SIMD SSE4_2 %i\n", _SIMD_config.has_SSE4_2);
+    printf("SIMD SSE4_1%i\n", _SIMD_config.has_SSE4_1);
+    printf("SIMD SSSE3 %i\n", _SIMD_config.has_SSSE3);
+    printf("SIMD SSE3 %i\n", _SIMD_config.has_SSE3);
+    printf("SIMD SSE2 %i\n", _SIMD_config.has_SSE1);
+    printf("SIMD SSE1 %i\n", _SIMD_config.has_SSE1);
+#endif
+
     int iset = instrset_detect(); // Detect supported instruction set
     // Choose which version of the entry function we want to point to:
 
+#if DEBUG_DISPATCH
     printf("ISET: %i", iset);
+#endif
+
     if ((iset >= 9) && (_SIMD_config.has_AVX512))
     {
-        #if DISTOPIA_AVX512_AVAILABLE
-        printf("\nRuntime dispatch AVX512\n");
+#if DISTOPIA_AVX512_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_AVX512::CalcBondsOrtho; // AVX512
-        #endif
-
+#endif
     }
-    else if  ((iset >= 8) && (_SIMD_config.has_AVX2))
+    else if ((iset >= 8) && (_SIMD_config.has_AVX2))
     {
-        #if DISTOPIA_AVX2_AVAILABLE
-        printf("\nRuntime dispatch AVX2\n");
+#if DISTOPIA_AVX2_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_AVX2::CalcBondsOrtho; // AVX2
-        #endif
+#endif
     }
     else if ((iset >= 7) && (_SIMD_config.has_AVX))
     {
-        #if DISTOPIA_AVX_AVAILABLE
-        printf("\nRuntime dispatch AVX\n");
+#if DISTOPIA_AVX_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_AVX::CalcBondsOrtho; // AVX
-        #endif
+#endif
     }
     else if ((iset >= 6) && (_SIMD_config.has_SSE4_2))
     {
-        #if DISTOPIA_SSE4_2_AVAILABLE
-        printf("\nRuntime dispatch SSE4.2\n");
+#if DISTOPIA_SSE4_2_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSE4_2::CalcBondsOrtho; // SSE4.2
-        #endif
+#endif
     }
     else if ((iset >= 5) && (_SIMD_config.has_SSE4_1))
     {
-        #if DISTOPIA_SSE4_1_AVAILABLE
-        printf("\nRuntime dispatch SSE4.1\n");
+#if DISTOPIA_SSE4_1_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSE4_1::CalcBondsOrtho; // SSE4.1
-        #endif
+#endif
     }
     else if ((iset >= 4) && (_SIMD_config.has_SSSE3))
     {
-        #if DISTOPIA_SSSE3_AVAILABLE
-        printf("\nRuntime dispatch SSSE3\n");
+#if DISTOPIA_SSSE3_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSSE3::CalcBondsOrtho; // SSSE3
-        #endif
+#endif
     }
     else if ((iset >= 3) && (_SIMD_config.has_SSE3))
     {
-        #if DISTOPIA_SSE3_AVAILABLE 
-        printf("\nRuntime dispatch SSE3\n");
+#if DISTOPIA_SSE3_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSE3::CalcBondsOrtho; // SSE3
-        #endif
+#endif
     }
     else if ((iset >= 2) && (_SIMD_config.has_SSE2))
     {
-        #if DISTOPIA_SSE2_AVAILABLE 
-        printf("\nRuntime dispatch SSE2\n");
+#if DISTOPIA_SSE2_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSE2::CalcBondsOrtho; // SSE2
-        #endif
+#endif
     }
     else if ((iset >= 1) && (_SIMD_config.has_SSE1))
     {
-        // this should always be the case
-        #if DISTOPIA_SSE1_AVAILABLE
-        printf("\nRuntime dispatch SSE1\n");
+// this should always be the case
+#if DISTOPIA_SSE1_AVAILABLE
         CalcBondsOrthoD_pointer = &Ns_SSE1::CalcBondsOrtho; // SSE1
-        #endif
+#endif
     }
     // continue in the dispatched version of the entry function
     (*CalcBondsOrthoD_pointer)(coords0, coords1, box, n, out);
 }
 
-// Call the entry function through the function pointer.
+// Call the entry function through the function pointer. This is a specialisation
+// of the template in /include/distopia.h
+
 // The first time this function is called, it goes through the dispatcher.
 // The dispatcher will change the function pointer so that all subsequent
 // calls go directly to the optimal version of the entry function
@@ -262,7 +266,6 @@ template <>
 void CalcBondsOrtho(const float *coords0, const float *coords1,
                     const float *box, std::size_t n, float *out)
 {
-    printf("Function entry: float\n");
     (*CalcBondsOrthoF_pointer)(coords0, coords1, box, n, out); // go to dispatched version
 }
 
@@ -270,10 +273,8 @@ template <>
 void CalcBondsOrtho(const double *coords0, const double *coords1,
                     const double *box, std::size_t n, double *out)
 {
-    printf("Function_entry: double\n");
     (*CalcBondsOrthoD_pointer)(coords0, coords1, box, n, out); // go to dispatched version
 }
-
 
 #endif // DISTOPIA_USE_SSE1
 #endif // DISTOPIA_DISPATCH
