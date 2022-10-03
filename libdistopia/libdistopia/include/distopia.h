2a1bf3958ea45d62e0175da4cfd91880edbc1c56
hmacdope <hugomacdermott@gmail.com>
2022-10-03 15:21:29 +1100

    fix dispatch

--- libdistopia/libdistopia/include/distopia.h
+++ libdistopia/libdistopia/include/distopia.h
@@ -3,12 +3,12 @@
 
 #include <cstddef>
 
-#ifdef DISTOPIA_DISPATCH
+#if DISTOPIA_DISPATCH
 #include "simd_dispatch.h"
 #endif
 
 
-#ifdef DISTOPIA_DISPATCH
+#if DISTOPIA_DISPATCH
 namespace DISPATCHED_NAMESPACE
 {
 #endif // DISTOPIA_DISPATCH
@@ -78,8 +78,73 @@ void CalcBondsIdxNoBox(const T *coords, const std::size_t *idxs, std::size_t n,
                        T *out);
 
 
-#ifdef DISTOPIA_DISPATCH
+#if DISTOPIA_DISPATCH
 } // DISPATCHED_NAMESPACE
 #endif // DISTOPIA_DISPATCH
+
+/*! \file 
+    \brief Main public header for distopia
+    
+    Contains functions to calculate distances, angles and dihedrals that
+    efficently leverage SIMD 
+*/
+
+
+/*!
+    \brief     Calculates the distance between two sets of points in two
+               contiguous arrays under orthorhombic periodic boundary
+               conditions.
+    \tparam    T the type of the coordinates (float or double)
+    \param     coords0 array of coordinates
+    \param     coords1 array of coordinates
+    \param     box the bounding box of the simulation
+    \param     n the number of distances to calculate
+    \param     out result array into which the distances are returned
+*/
+template <typename T>
+void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                    std::size_t n, T *out);
+
+/*!
+    \brief     Calculates the distance between two sets of points in two
+               contiguous arrays without periodic boundary conditions
+    \tparam    T the type of the coordinates (float or double)
+    \param     coords0 array of coordinates
+    \param     coords1 array of coordinates
+    \param     n the number of distances to calculate
+    \param     out result array into which the distances are returned
+*/
+template <typename T>
+void CalcBondsNoBox(const T *coords0, const T *coords1, std::size_t n, T *out);
+
+/*!
+    \brief     Calculates the distance between sets of two points in a single
+               array indexed by the idxs parameter under orthorhombic periodic
+               boundary conditions
+    \tparam    T the type of the coordinates (float or double)
+    \param     coords array of coordinates
+    \param     idxs the indicies of the distances to calculate
+    \param     box the bounding box of the simulation
+    \param     n the number of distances to calculate
+    \param     out result array into which the distances are returned
+*/
+template <typename T>
+void CalcBondsIdxOrtho(const T *coords, const std::size_t *idxs, const T *box,
+                       std::size_t n, T *out);
+
+/*!
+    \brief     Calculates the distance between sets of two points in a single
+               array indexed by the idxs parameter without periodic boundary
+               conditions
+    \tparam    T the type of the coordinates (float or double)
+    \param     coords array of coordinates
+    \param     idxs the indicies of the distances to calculate
+    \param     n the number of distances to calculate
+    \param     out result array into which the distances are returned
+*/
+template <typename T>
+void CalcBondsIdxNoBox(const T *coords, const std::size_t *idxs, std::size_t n,
+                       T *out);
+
 
 #endif // DISTOPIA_H
--- libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
+++ libdistopia/libdistopia/lib/calc_bonds_dispatch.cpp
@@ -3,6 +3,7 @@
 
 #include "simd_dispatch.h"
 #include "vectorclass.h"
+#include "../include/distopia.h"
 
 #include <cstddef>
 #include <iostream>
@@ -10,56 +11,70 @@
 typedef void CalcBondsOrthoFT(const float *coords0, const float *coords1,
                               const float *box, std::size_t n, float *out);
 
-CalcBondsOrthoFT CalcBondsOrtho, CalcBondsOrthoDispatch;
+typedef void CalcBondsOrthoDT(const double *coords0, const double *coords1,
+                              const double *box, std::size_t n, double *out);
+
+CalcBondsOrthoFT CalcBondsOrtho, CalcBondsOrthoDispatchF;
+CalcBondsOrthoDT CalcBondsOrtho, CalcBondsOrthoDispatchD;
 
 // Define function prototypes
 namespace Ns_SSE1
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_SSE2
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_SSE3
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_SSSE3
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_SSE4_1
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_SSE4_2
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
 namespace Ns_AVX
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 namespace Ns_AVX2
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 namespace Ns_AVX512
 {
     CalcBondsOrthoFT CalcBondsOrtho;
+    CalcBondsOrthoDT CalcBondsOrtho;
 };
 
-CalcBondsOrthoFT *CalcBondsOrthoF_pointer = &CalcBondsOrthoDispatch; // function pointer
+CalcBondsOrthoFT *CalcBondsOrthoF_pointer = &CalcBondsOrthoDispatchF; // function pointer
+CalcBondsOrthoDT *CalcBondsOrthoD_pointer = &CalcBondsOrthoDispatchD; // function pointer
 
 // Dispatch function
-void CalcBondsOrthoDispatch(const float *coords0, const float *coords1,
+void CalcBondsOrthoDispatchF(const float *coords0, const float *coords1,
                             const float *box, std::size_t n, float *out)
 {
     int iset = instrset_detect(); // Detect supported instruction set
@@ -118,15 +133,90 @@ void CalcBondsOrthoDispatch(const float *coords0, const float *coords1,
     (*CalcBondsOrthoF_pointer)(coords0, coords1, box, n, out);
 }
 
+
+// Dispatch function
+void CalcBondsOrthoDispatchD(const double *coords0, const double *coords1,
+                    const double *box, std::size_t n, double *out)
+{
+    int iset = instrset_detect(); // Detect supported instruction set
+    // Choose which version of the entry function we want to point to:
+    if (iset >= 9)
+    {
+        printf("\nRuntime dispatch AVX512\n");
+        CalcBondsOrthoD_pointer = &Ns_AVX512::CalcBondsOrtho; // AVX512
+    }
+    else if (iset >= 8)
+    {
+        printf("\nRuntime dispatch AVX2\n");
+        CalcBondsOrthoD_pointer = &Ns_AVX2::CalcBondsOrtho; // AVX2
+    }
+    else if (iset >= 7)
+    {
+        printf("\nRuntime dispatch AVX\n");
+        CalcBondsOrthoD_pointer = &Ns_AVX::CalcBondsOrtho; // AVX
+    }
+    else if (iset >= 6)
+    {
+        printf("\nRuntime dispatch SSE4.2\n");
+        CalcBondsOrthoD_pointer = &Ns_SSE4_2::CalcBondsOrtho; // SSE4.2
+    }
+    else if (iset >= 5)
+    {
+        printf("\nRuntime dispatch SSE4.1\n");
+        CalcBondsOrthoD_pointer = &Ns_SSE4_1::CalcBondsOrtho; // SSE4.1
+    }
+    else if (iset >= 4)
+    {
+        printf("\nRuntime dispatch SSSE3\n");
+        CalcBondsOrthoD_pointer = &Ns_SSSE3::CalcBondsOrtho; // SSSE3
+    }
+    else if (iset >= 3)
+    {
+        printf("\nRuntime dispatch SSE3\n");
+        CalcBondsOrthoD_pointer = &Ns_SSE3::CalcBondsOrtho; // SSE3
+    }
+    else if (iset >= 2)
+    {
+        printf("\nRuntime dispatch SSE2\n");
+        CalcBondsOrthoD_pointer = &Ns_SSE2::CalcBondsOrtho; // SSE2
+    }
+    else if (iset >= 1)
+    {
+        printf("\nRuntime dispatch SSE1\n");
+        CalcBondsOrthoD_pointer = &Ns_SSE1::CalcBondsOrtho; // SSE1
+    }
+    else
+    {
+        // Error: lowest instruction set not supported.
+        std::cerr << "Error: Minimum instruction set SSE1 not supported on this computer";
+    }
+    // continue in the dispatched version of the entry function
+    (*CalcBondsOrthoD_pointer)(coords0, coords1, box, n, out);
+}
+
 // Call the entry function through the function pointer.
 // The first time this function is called, it goes through the dispatcher.
 // The dispatcher will change the function pointer so that all subsequent
 // calls go directly to the optimal version of the entry function
+
+template <typename T>
+void CalcBondsOrtho(const T *coords0, const T *coords1, const T *box,
+                    std::size_t n, T *out);
+
+template <>
 void CalcBondsOrtho(const float *coords0, const float *coords1,
                     const float *box, std::size_t n, float *out)
 {
     (*CalcBondsOrthoF_pointer)(coords0, coords1, box, n, out); // go to dispatched version
 }
+
+template <>
+void CalcBondsOrtho(const double *coords0, const double *coords1,
+                    const double *box, std::size_t n, double *out)
+{
+    (*CalcBondsOrthoD_pointer)(coords0, coords1, box, n, out); // go to dispatched version
+}
+
 
 #endif // DISTOPIA_USE_SSE1
 #endif // DISTOPIA_DISPATCH
